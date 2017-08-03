630b39ba744131110b8ede62f65e2607a281fc3c
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-08-03 13:07:49 +1000

    Updated to version 0.6.4.5

--- peek_plugin_livedb/__init__.py
+++ peek_plugin_livedb/__init__.py
@@ -5,7 +5,7 @@ from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHo
 from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
 from peek_plugin_base.worker.PluginWorkerEntryHookABC import PluginWorkerEntryHookABC
 
-__version__ = '0.6.4'
+__version__ = '0.6.4.5'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:

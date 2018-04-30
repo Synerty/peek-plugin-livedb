248c19acc4066e12d790310f170ec0e63cb3abba
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-30 23:38:37 +1000

    Updated for compatibility with new PayloadEnvelope Vortex update
    ORMOB-614

--- peek_plugin_livedb/_private/server/tuple_providers/StringIntTupleProvider.py
+++ peek_plugin_livedb/_private/server/tuple_providers/StringIntTupleProvider.py
@@ -1,20 +1,24 @@
+import logging
+
 from txhttputil.util.DeferUtil import deferToThreadWrap
 from typing import Union
 
 from twisted.internet.defer import Deferred
 
+from vortex.DeferUtil import deferToThreadWrapWithLogger
 from vortex.Payload import Payload
 from vortex.TupleSelector import TupleSelector
 from vortex.handler.TupleDataObservableHandler import TuplesProviderABC
 
 from peek_plugin_livedb._private.storage.StringIntTuple import StringIntTuple
 
+logger = logging.getLogger(__name__)
 
 class StringIntTupleProvider(TuplesProviderABC):
     def __init__(self, ormSessionCreator):
         self._ormSessionCreator = ormSessionCreator
 
-    @deferToThreadWrap
+    @deferToThreadWrapWithLogger(logger)
     def makeVortexMsg(self, filt: dict,
                       tupleSelector: TupleSelector) -> Union[Deferred, bytes]:
         # Potential filters can be placed here.
@@ -29,7 +33,7 @@ class StringIntTupleProvider(TuplesProviderABC):
             )
 
             # Create the vortex message
-            return Payload(filt, tuples=tasks).toVortexMsg()
+            return Payload(filt, tuples=tasks).makePayloadEnvelope().toVortexMsg()
 
         finally:
             session.close()

df7a59fdf95b3fa72b68c3adba9da414f8413584
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-06 08:17:07 +1000

    Updated to version 1.0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ author = "Synerty"
 author_email = 'contact@synerty.com'
 py_package_name = "peek_plugin_livedb"
 pip_package_name = py_package_name.replace('_', '-')
-package_version = '1.0.0.3'
+package_version = '1.0.0.4'
 description = 'Peek Plugin LiveDB - My first enhancement.'
 
 download_url = 'https://bitbucket.org/synerty/%s/get/%s.zip'

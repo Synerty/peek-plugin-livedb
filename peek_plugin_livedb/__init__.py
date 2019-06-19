b475d334e36e67065510b9a3c7dccf056cf976b7
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:26 +1000

    Updated package publish scripts.
    PEEK-321

--- peek_plugin_livedb/__init__.py
+++ peek_plugin_livedb/__init__.py
@@ -5,7 +5,7 @@ from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHo
 from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
 from peek_plugin_base.worker.PluginWorkerEntryHookABC import PluginWorkerEntryHookABC
 
-__version__ = '1.3.2'
+__version__ = '0.0.0'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
---
+++ publish.settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_plugin_livedb"
+PYPI_PUBLISH="1"
+
+VER_FILES=""

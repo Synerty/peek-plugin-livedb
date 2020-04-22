420189eed6217d3950b97250605a1b212bfb2339
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-04-22 19:18:37 +1000

    bug: The livedb queue can now take nulls
    PEEK-550

--- peek_plugin_livedb/_private/storage/LiveDbRawValueQueue.py
+++ peek_plugin_livedb/_private/storage/LiveDbRawValueQueue.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Optional
 
-from sqlalchemy import Column, BigInteger
+from sqlalchemy import Column, BigInteger, Index
 from sqlalchemy import Integer, String
 from vortex.Tuple import Tuple, addTupleType
 
@@ -16,15 +16,14 @@ logger = logging.getLogger(__name__)
 @addTupleType
 class LiveDbRawValueQueue(Tuple, DeclarativeBase,
                           ACIProcessorQueueTupleABC):
-
     __tablename__ = 'LiveDbRawValueQueue'
     __tupleType__ = livedbTuplePrefix + __tablename__
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
 
-    modelSetId = Column(Integer, primary_key=True)
-    key = Column(String, primary_key=True)
-    rawValue = Column(String, primary_key=True)
+    modelSetId = Column(Integer, nullable=False)
+    key = Column(String, nullable=False)
+    rawValue = Column(String)
 
     @classmethod
     def sqlCoreLoad(cls, row):
@@ -33,6 +32,10 @@ class LiveDbRawValueQueue(Tuple, DeclarativeBase,
     def ckiUniqueKey(self):
         """ See LiveDbRawValueQueueTuple.ckiUniqueKey """
         raise NotImplementedError()
+
+    __table_args__ = (
+        Index("idx_LiveDbRawValueQueue_all", id, modelSetId, key, rawValue, unique=False),
+    )
 
 
 @addTupleType
